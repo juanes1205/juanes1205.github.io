6aed18519ed8fe3e70e59a6a0047c0857adf8b86
Esteban <juanes_11@hotmail.com>
2022-03-30 16:25:32 -0500

    Commit 2

--- js/lang.js
+++ js/lang.js
@@ -1,14 +1,67 @@
 //Espanõl
 $(function() {
     $(".es").click(function() {
-        //encabezamiento
+        //Menu
         $(".nav-item").children().eq(0).text("PERFIL PROFESIONAL");
         $(".nav-item").children().eq(1).text("EXPERIENCIA");
-        $(".nav-item").children().eq(2).text("Comprar");
-        $(".nav-item").children().eq(3).text("Entra");
+        $(".nav-item").children().eq(2).text("EDUCACIÓN");
+        $(".nav-item").children().eq(3).text("CURSOS");
+        $(".nav-item").children().eq(4).text("HABILIDADES Y DESTREZAS");
+        //Idioma
         $(".lang").children().eq(0).text("Español");
         $(".lang").children().eq(1).text("Ingles");
+        //Perfil
         $("#career_profile_text").text("Estudiante de séptimo semestre de Ingeniería de Sistemas de la Institución Universitaria de Envigado. Con conocimiento en bases de datos SQL Server, MySQL y Oracle, programación web en PHP, aplicaciones de escritorio en JAVA y C#. Además de ser proactivo, responsable, con iniciativa y puntualidad, asumo todos aquellos retos y metas que la organización me pueda plantear; con buen manejo de relaciones interpersonales, facilidad para trabajar en equipo, así como para resolver problemas eficientemente y con facilidad para el rápido aprendizaje ante los cambios constantes de la tecnología");
+        //Experiencia
+        $("#experience_title").text("EXPERIENCIA");
+        $("#experience_job1").text("AUXILIAR DE SISTEMAS");
+        $("#experience_date1").text("Noviembre 2017 - Presente");
+        //Educación
+        $("#education_title").text("EDUCACIÓN");
+        $("#education_school1").text("Institución Universitaria de Envigado");
+        $("#education_college_career1").text("Ingeniería de Sistemas");
+        $("#education_date1").text("Febrero 2022 - Presente");
+        $("#education_school2").text("Institución Universitaria de Envigado");
+        $("#education_college_career2").text("Tecnología en Desarrollo de Sistemas de Información");
+        $("#education_date2").text("Julio 2018 - Noviembre 2021");
+        $("#education_school3").text("Centro de Sistemas de Antioquia (CENSA)");
+        $("#education_college_career3").text("Técnico Laboral en Análisis y Programación de Computadores");
+        $("#education_date3").text("Febrero 2017 - Agosto 2018");
+        //Cursos
+        $("#course_name1").text("Desarrollo web profesional en PHP con Laravel 7");        
+        $("#course_date1").text("Actualmente");
+        $("#course_name2").text("Desarrollo de sistemas web en PHP 7 POO, MySQL, JQuery Ajax");       
+        $("#course_date2").text("Actualmente");
+        $("#course_name3").text("Crea Sitios Joomla Extraordinarios para Empresas con Gantry");        
+        $("#course_date3").text("Marzo 2022");
+        $("#course_name4").text("Joomla 3 Mi primer proyecto con Joomla");       
+        $("#course_date4").text("Diciembre 2021");
+        $("#course_name5").text("Desarrollo web en PHP con Laravel 5.6, VueJS y MariaDB MySQL");       
+        $("#course_date5").text("Mayo 2021");
+        $("#course_name6").text("Desarrollo Web con PHP");       
+        $("#course_date6").text("Mayo 2020");
+        $("#course_name7").text("CCNA Routing and Switching: Principios básicos de routing y switching");
+        $("#course_platform7").text("Institución Universitaria de Envigado");
+        $("#course_date7").text("Julio 2020");
+        $("#course_name8").text("CCNA Routing and Switching: Introducción a redes");
+        $("#course_platform8").text("Institución Universitaria de Envigado");
+        $("#course_date8").text("Noviembre 2019");
+        $("#course_name9").text("Administración de Servidor de Copias Seguridad");
+        $("#course_platform9").text("Centro de Sistemas de Antioquia (CENSA)");
+        $("#course_date9").text("Julio 2018");
+        $("#course_name10").text("Variables y estructuras de control en la programación orientada a objetos: java");      
+        $("#course_date10").text("Noviembre 2016");
+        $("#course_name11").text("Bases de Datos Generalidades y Sistemas de Gestión");       
+        $("#course_date11").text("Octubre 2016");
+        $("#course_name12").text("Calidad en el Desarrollo de Software");      
+        $("#course_date12").text("Septiembre 2016");
+        $("#course_name13").text("Metodología de la Programación de Sistemas Informáticos");   
+        $("#course_date13").text("Julio 2016");
+        //Habilidades y Destrezas
+        $("#skills_title").text("HABILIDADES Y DESTREZAS");
+        $("#skills_tools").text("Herramientas y lenguajes de programación");
+
+
 
 
         $(".language-selected").text("es");
@@ -30,14 +83,67 @@ $(function() {
 //English
 $(function() {
     $(".en").click(function() {
-        //header
+        //Menu
         $(".nav-item").children().eq(0).text("CAREER PROFILE");
         $(".nav-item").children().eq(1).text("EXPERIENCE");
-        $(".nav-item").children().eq(2).text("Buy");
-        $(".nav-item").children().eq(3).text("Log in");
+        $(".nav-item").children().eq(2).text("EDUCATION");
+        $(".nav-item").children().eq(3).text("COURSES");
+        $(".nav-item").children().eq(4).text("SKILLS & PROFICIENCY");
+        //Language
         $(".lang").children().eq(0).text("Spanish");
         $(".lang").children().eq(1).text("English");
+        //Profile
         $("#career_profile_text").text("Seventh-semester student of Systems Engineering at the Envigado University Institution. With knowledge in SQL Server, MySQL and Oracle databases, web programming in PHP, desktop applications in JAVA and C#. In addition to being proactive, responsible, with initiative and punctuality, I assume all those challenges and goals that the organization may set for me; with good handling of interpersonal relationships, ability to work in a team, as well as to solve problems efficiently and with the ability to learn quickly in the face of constant changes in technology.");
+        //Experience
+        $("#experience_title").text("EXPERIENCE");
+        $("#experience_job1").text("SYSTEMS ASSISTANT");
+        $("#experience_date1").text("November 2017 - Present");
+        //Education
+        $("#education_title").text("EDUCATION");
+        $("#education_school1").text("University Institution of Envigado");
+        $("#education_college_career1").text("Systems engineer");
+        $("#education_date1").text("February 2022 - Present");
+        $("#education_school2").text("University Institution of Envigado");
+        $("#education_college_career2").text("Information Systems Development Technology");
+        $("#education_date2").text("July 2018 - November 2021");
+        $("#education_school3").text("Systems Center of Antioquia (CENSA)");
+        $("#education_college_career3").text("Labor Technician in Computer Analysis and Programming");
+        $("#education_date3").text("February 2017 - August 2018");
+        //Courses
+        $("#course_name1").text("Professional web development in PHP with Laravel 7");        
+        $("#course_date1").text("Present");
+        $("#course_name2").text("Development of web systems in PHP 7 OOP, MySQL, JQuery Ajax");        
+        $("#course_date2").text("Present");
+        $("#course_name3").text("Create Extraordinary Joomla Sites for Businesses with Gantry");        
+        $("#course_date3").text("March 2022");
+        $("#course_name4").text("Joomla 3 My first project with Joomla");        
+        $("#course_date4").text("December 2021");
+        $("#course_name5").text("Web development in PHP with Laravel 5.6, VueJS and MariaDB MySQL");        
+        $("#course_date5").text("May 2021");
+        $("#course_name6").text("Web development with PHP");       
+        $("#course_date6").text("May 2020");
+        $("#course_name7").text("CCNA Routing and Switching: Routing and Switching Basics");
+        $("#course_platform7").text("University Institution of Envigado");
+        $("#course_date7").text("July 2020");
+        $("#course_name8").text("CCNA Routing and Switching: Introduction to Networks");
+        $("#course_platform8").text("University Institution of Envigado");
+        $("#course_date8").text("November 2019");
+        $("#course_name9").text("Backup Server Administration");
+        $("#course_platform9").text("Systems Center of Antioquia (CENSA)");
+        $("#course_date9").text("July 2018");
+        $("#course_name10").text("Variables and control structures in object-oriented programming: java");       
+        $("#course_date10").text("November 2016");
+        $("#course_name11").text("General Databases and Management Systems");        
+        $("#course_date11").text("October 2016");
+        $("#course_name12").text("Quality in Software Development");        
+        $("#course_date12").text("September 2016");
+        $("#course_name13").text("Computer Systems Programming Methodology");       
+        $("#course_date13").text("July 2016");
+        //Skills & Proficiency
+        $("#skills_title").text("SKILLS & PROFICIENCY");
+        $("#skills_tools").text("Programming Languages & Tools");
+
+    
 
 
 
